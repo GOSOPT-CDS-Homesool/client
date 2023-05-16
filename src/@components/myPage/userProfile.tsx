f198650e8a46377955220296556e7c9f053d5b32
Seojisoo20191941 <amy0480@naver.com>
2023-05-16 22:31:27 +0900

    [ design ] 유저 프로필 구현

--- src/@components/myPage/userProfile.tsx
+++ src/@components/myPage/userProfile.tsx
@@ -3,17 +3,42 @@ import { styled } from "styled-components";
 
 export default function UserProfile() {
   return (
-    <>
+    <UserProfileContainer>
       <MyPageUserProfileImgIc />
-      <Name>이호재님</Name>
-      <RankWrapper>
-        <p>이번달 등급</p>
-        <p>브론즈</p>
-      </RankWrapper>
-      <MyPageUpdateProfileIc />
-    </>
+      <UserProfileWrapper>
+        <FlexBox width={28.5}>
+          <Name>이호재님</Name>
+          <MyPageUpdateProfileIc />
+        </FlexBox>
+        <FlexBox width={9.9}>
+          <RankTitle>이번달 등급</RankTitle>
+          <p>브론즈</p>
+        </FlexBox>
+      </UserProfileWrapper>
+    </UserProfileContainer>
   );
 }
+
+const FlexBox = styled.section<{ width: number }>`
+  display: flex;
+  justify-content: space-between;
+
+  width: ${({ width }) => width}rem;
+  margin-left: 1.2rem;
+`;
+const UserProfileContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 34.3rem;
+  height: 4.8rem;
+`;
+
+const UserProfileWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
 
 const Name = styled.h1`
   ${({ theme }) => theme.fonts.head01}
@@ -22,4 +47,9 @@ const Name = styled.h1`
 const RankWrapper = styled.div`
   display: flex;
   ${({ theme }) => theme.fonts.body02}
+`;
+
+const RankTitle = styled.p`
+  margin-right: 0.4rem;
+  color: ${({ theme }) => theme.colors.GRAYSCALE500};
 `;
