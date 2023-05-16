6cdc9b63f8a52f52e46d4f3c5daa90f8034d1734
Seojisoo20191941 <amy0480@naver.com>
2023-05-16 22:03:52 +0900

    [ feat ] svg 파일 삽입

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,3 @@
 export { ReactComponent as MainHomeSoolIc } from "./icon/homesool.svg";
+export { ReactComponent as MyPageUserProfileImgIc } from "./icon/myPageUserProfileImgIc.svg";
+export { ReactComponent as MyPageUpdateProfileIc } from "./icon/myPageUpdateProfileIc.svg";
