44a72ffb98a421ebb71a1c7ef3af3e6f9f2f0383
Seojisoo20191941 <amy0480@naver.com>
2023-05-16 22:02:00 +0900

    [ feat ] 프로필 컴포넌트 구현

---
+++ src/@components/myPage/userProfile.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { MyPageUpdateProfileIc, MyPageUserProfileImgIc } from "../../assets";
+import { styled } from "styled-components";
+import { theme } from "../../style/theme";
+
+export default function UserProfile() {
+  return (
+    <>
+      <MyPageUserProfileImgIc />
+      <Name>이호재님</Name>
+      <RankWrapper>
+        <p>이번달 등급</p>
+        <p>브론즈</p>
+      </RankWrapper>
+      <MyPageUpdateProfileIc />
+    </>
+  );
+}
+
+const Name = styled.h1`
+  ${({ theme }) => theme.fonts.head01}
+`;
+
+const RankWrapper = styled.div`
+  display: flex;
+  ${({ theme }) => theme.fonts.body02}
+`;
