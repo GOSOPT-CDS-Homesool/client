0d085c1730488284b703732f45fc97d63b1d499d
Seojisoo20191941 <amy0480@naver.com>
2023-05-16 23:59:20 +0900

    [ etc ] 타입 에러 수정 시도중

--- src/@components/myPage/couponInfoBox.tsx
+++ src/@components/myPage/couponInfoBox.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { COUPON_LIST } from "../../core/myPage";
+import CouponInfo from "./couponInfo";
 
 export default function CouponInfoBox() {
-  return ();
+  return <>{COUPON_LIST.map({ id:number, title:string })=><CouponInfo key={id} title={title}/>}</>;
 }
