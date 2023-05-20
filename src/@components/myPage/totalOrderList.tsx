96ce3888341f0acf63a08f03a2ff723264ff26f1
Seojisoo20191941 <amy0480@naver.com>
2023-05-20 14:10:02 +0900

    [ feat ] 서브타이틀 구현

--- src/@components/myPage/totalOrderList.tsx
+++ src/@components/myPage/totalOrderList.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 import { OrderListType } from "../../type/mypage";
 import Title from "../common/title";
+import { styled } from "styled-components";
 
 export default function TotalOrderList(props: OrderListType) {
   const { orderList } = props;
 
   return (
     <>
-      <Title title="전체 주문 내역" />
+      <TitleWrapper>
+        <Title title="전체 주문 내역" />
+        <SubTitle>(최근 30일)</SubTitle>
+      </TitleWrapper>
       {/* {orderList.map(({ waiting, finish, ready, delivering, delivered })=>())} */}
     </>
   );
 }
+
+const SubTitle = styled.p`
+  margin-left: 0.4rem;
+
+  color: ${({ theme }) => theme.colors.GRAYSCALE500};
+  ${({ theme }) => theme.fonts.body02};
+`;
+
+const TitleWrapper = styled.header`
+  display: flex;
+  align-items: center;
+`;
