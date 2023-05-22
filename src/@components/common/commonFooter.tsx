9cd02d73f722510e2f176453f6c0d197512f5f68
Seojisoo20191941 <amy0480@naver.com>
2023-05-22 23:53:40 +0900

    feat: 클릭하면 하도록 구현

--- src/@components/common/commonFooter.tsx
+++ src/@components/common/commonFooter.tsx
@@ -1,10 +1,43 @@
-import React from "react";
-import { CommonFooterIc } from "../../assets";
+import { useState } from "react";
+import { CommonFooterIc, FooterHomeActiveIc, FooterHomeIc, FooterMyActiveIc, FooterMyIc } from "../../assets";
 import { styled } from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 export default function CommonFooter() {
-  return <CommonFooterIcon />;
+  const [isHome, setIsHome] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+  function activateHome() {
+    setIsHome(true);
+    navigate("/");
+  }
+
+  function activateMy() {
+    setIsHome(false);
+    navigate("/mypage");
+  }
+
+  return (
+    <>
+      <ButtonWrapper>
+        <div onClick={activateHome}>{isHome ? <FooterHomeActiveIc /> : <FooterHomeIc />}</div>
+        <div onClick={activateMy}>{isHome ? <FooterMyIc /> : <FooterMyActiveIc />}</div>
+      </ButtonWrapper>
+      <CommonFooterIcon />
+    </>
+  );
 }
+
+const ButtonWrapper = styled.section`
+  display: flex;
+
+  position: fixed;
+  z-index: 2;
+  top: 63.8rem;
+  margin-left: 10rem;
+
+  cursor: pointer;
+`;
 
 const CommonFooterIcon = styled(CommonFooterIc)`
   position: fixed;
