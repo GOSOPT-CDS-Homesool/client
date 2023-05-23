a42fcb5a041139df1af2049e974f0f794e962aa3
Seojisoo20191941 <amy0480@naver.com>
2023-05-24 00:01:14 +0900

    [ design ] 헤더 위치 조정

--- src/@components/common/header.tsx
+++ src/@components/common/header.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { HomeHeaderIc } from "../../assets";
+import { styled } from "styled-components";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <HeaderWRapper>
+      <HomeHeaderIc />
+    </HeaderWRapper>
+  );
 }
+
+const HeaderWRapper = styled.header`
+  margin-left: -1.65rem;
+`;
--- src/@pages/home.tsx
+++ src/@pages/home.tsx
@@ -4,10 +4,12 @@ import Review from "../@components/homePage/review";
 import Subscribe from "../@components/homePage/subscribe";
 import ThisMonth from "../@components/homePage/thisMonth";
 import Twinkle from "../@components/homePage/twinkle";
+import Header from "../@components/common/header";
 
 export default function Home() {
   return (
     <>
+      <Header />
       <ThisMonth />
       <Twinkle />
       <Subscribe />
