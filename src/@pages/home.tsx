3ab42c16cbe9a93c8d58ea711ab9acf4360bf13c
Seojisoo20191941 <amy0480@naver.com>
2023-05-24 02:03:46 +0900

    [ feat ] 홈페이지 페이지 푸터, 푸터 삽입

--- src/@pages/home.tsx
+++ src/@pages/home.tsx
@@ -6,6 +6,8 @@ import ThisMonth from "../@components/homePage/thisMonth";
 import Twinkle from "../@components/homePage/twinkle";
 import Header from "../@components/common/header";
 import Banner from "../@components/homePage/banner";
+import PageFooter from "../@components/common/pageFooter";
+import CommonFooter from "../@components/common/commonFooter";
 
 export default function Home() {
   return (
@@ -17,6 +19,8 @@ export default function Home() {
       <Subscribe />
       <BrandNew />
       <Review />
+      <PageFooter />
+      <CommonFooter />
     </>
   );
 }
