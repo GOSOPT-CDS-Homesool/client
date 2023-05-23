88d198c7b7f4dc1b3ad3075d63c67544a869961e
MyungJi_Jang <myungvely712@hufs.ac.kr>
2023-05-23 12:30:43 +0900

    [ design ] 주문상품UI

--- src/@components/orderPaymentPage/orderConfirmation/orderConfirmation.tsx
+++ src/@components/orderPaymentPage/orderConfirmation/orderConfirmation.tsx
@@ -2,23 +2,29 @@ import { styled } from "styled-components";
 import { BackButtonIc } from "../../../assets";
 import ShippingAddress from "./shippingAddress";
 import OrdererInfo from "./ordererInfo";
+import OrderProductInfo from "./orderProductInfo";
 
 export default function OrderConfirmation() {
   return (
     <div>
       <DetailHeader>
-        <BackButtonIc />
-        <DetailTitle>주문결제</DetailTitle>
+        <BackButtonIcon />
+        <DetailTitle>주문/결제</DetailTitle>
       </DetailHeader>
       <ShippingAddress />
       <OrdererInfo />
+      <OrderProductInfo />
     </div>
   );
 }
 
+const BackButtonIcon = styled(BackButtonIc)`
+  margin: 0.2rem 0 0 -0.8rem;
+`;
+
 const DetailTitle = styled.h1`
   margin: 0.5rem 0 1.3rem 12.9rem;
-  ${({ theme }) => theme.fonts.body03}
+  ${({ theme }) => theme.fonts.body01}
 `;
 
 const DetailHeader = styled.header`
