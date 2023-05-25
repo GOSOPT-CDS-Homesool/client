17d15e369ff1e5bfc70ff207c7aa6ebdfeb19841
Seojisoo20191941 <amy0480@naver.com>
2023-05-26 03:43:52 +0900

    [ fix ] 에러 해결중

--- src/@pages/home.tsx
+++ src/@pages/home.tsx
@@ -6,7 +6,7 @@ import BrandNew from "../@components/homePage/brandNew";
 import Review from "../@components/homePage/review";
 import Subscribe from "../@components/homePage/subscribe";
 import ThisMonth from "../@components/homePage/thisMonth";
-import Twinkle from "../@components/homePage/twinkle";
+import Twinkle from "../@components/homePage/Twinkle";
 
 export default function Home() {
   return (
