57a313c397433ec2f05728d559209e02c92a75a6
Seojisoo20191941 <amy0480@naver.com>
2023-05-26 18:06:05 +0900

    [ fix ] 빌드 에러 수정

--- src/@pages/home.tsx
+++ src/@pages/home.tsx
@@ -6,7 +6,6 @@ import BrandNew from "../@components/homePage/brandNew";
 import Review from "../@components/homePage/review";
 import Subscribe from "../@components/homePage/subscribe";
 import ThisMonth from "../@components/homePage/thisMonth";
-import TopButton from "../@components/homePage/topButton";
 import Twinkle from "../@components/homePage/Twinkle";
 
 export default function Home() {
